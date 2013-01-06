672a10149058f21e897c72a2afc3d1ea5e8aad78
Ryan Galloway <ryan@rsgalloway.com>
2013-01-05 20:32:02 -0800

    changed default port to 5000

--- main.py
+++ main.py
@@ -68,4 +68,5 @@ def catch_all(path):
     return flask.send_from_directory(folder, filename)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
